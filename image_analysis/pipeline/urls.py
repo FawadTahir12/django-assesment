0cba52645944018e1632c0a3a244b726003b18fd
ftahir123 <fawad.tahir@alpharages.com>
2025-02-13 14:33:25 +0500

    Minor changes

--- image_analysis/pipeline/urls.py
+++ image_analysis/pipeline/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('image_analysis/', views.ImageAnalysisView.as_view(), name='image-analysis-view')
+    path('image-analysis/', views.ImageAnalysisView.as_view(), name='image-analysis-view')
 ]
